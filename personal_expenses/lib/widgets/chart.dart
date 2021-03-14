2d586e0f2c33be8f0c8d66a02b1a43fbcc4274cf
fadhilahm <fadhilahmetra@gmail.com>
2021-03-14 22:36:58 +0700

    Finished wiring the grouped transactions to Chart widget.

---
+++ personal_expenses/lib/widgets/chart.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      var totalSum = 0.0;
+      for (final transaction in recentTransactions) {
+        if (transaction.date.day == weekDay.day &&
+            transaction.date.month == weekDay.month &&
+            transaction.date.year == weekDay.year) {
+          totalSum += transaction.amount;
+        }
+      }
+
+      return {
+        'day': '${DateFormat.E().format(weekDay)}',
+        'amount': totalSum.toStringAsFixed(2)
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: groupedTransactionValues.map((singleDay) {
+          return Column(
+            children: [
+              Text(singleDay['day']),
+              Text(singleDay['amount'].toString()),
+            ],
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
