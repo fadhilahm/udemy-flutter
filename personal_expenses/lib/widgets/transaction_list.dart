818f97e66d1583309c0431ceacf44e5b55f27871
fadhilahm <fadhilahmetra@gmail.com>
2021-03-16 10:56:12 +0700

    Polished app

--- personal_expenses/lib/widgets/transaction_list.dart
+++ personal_expenses/lib/widgets/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 500,
+      height: 450,
       child: transactions.isEmpty
           ? Column(
               children: [
