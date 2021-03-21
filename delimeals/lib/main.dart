ec4aebf4845af0c40daab13f2aaef4c987ca3199
fadhilahm <fadhilahmetra@gmail.com>
2021-03-21 18:41:45 +0700

    Finished category-meals screen.

--- delimeals/lib/main.dart
+++ delimeals/lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp())
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
 
-class MyApp extends MaterialApp {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'DeliMeals',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            accentColor: Colors.amber,
+            canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+            fontFamily: 'Raleway',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText2: TextStyle(
+                  color: const Color.fromRGBO(20, 51, 51, 1),
+                ),
+                bodyText1: TextStyle(
+                  color: const Color.fromRGBO(20, 51, 51, 1),
+                ),
+                headline6: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed',
+                ))),
+        // home: CategoriesScreen(),
+        initialRoute: '/', // Default is already `/`.
+        routes: {
+          '/': (ctx) => CategoriesScreen(),
+          CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        },
+      );
 }
---
+++ delimeals/lib/screens/category_meals_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../dummy_data.dart';
+import '../widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String id;
+  // final String title;
+
+  // const CategoryMealsScreen(this.id, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryTitle = routeArgs['title'];
+    final categoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(categoryId))
+        .toList();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(categoryTitle),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, idx) => MealItem(
+              title: categoryMeals[idx].title,
+              imageUrl: categoryMeals[idx].imageUrl,
+              affordability: categoryMeals[idx].affordability,
+              complexity: categoryMeals[idx].complexity,
+              duration: categoryMeals[idx].duration),
+          itemCount: categoryMeals.length,
+        ));
+  }
+}
