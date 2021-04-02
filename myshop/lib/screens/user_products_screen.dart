a191a91cb0348ef72f704f21e009e1c77e969c25
fadhilahm <fadhilahmetra@gmail.com>
2021-04-02 22:57:12 +0700

    Added edit product screen.

--- myshop/lib/screens/user_products_screen.dart
+++ myshop/lib/screens/user_products_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/main_drawer.dart';
+import '../screens/edit_product_screen.dart';
+
 import '../widgets/user_product_item.dart';
+import '../widgets/main_drawer.dart';
 
 import '../providers/products.dart';
 
@@ -14,6 +16,14 @@ class UserProductsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () => Navigator.of(context).pushNamed(
+              EditProductScreen.routeName,
+            ),
+          )
+        ],
       ),
       drawer: MainDrawer(),
       body: Padding(
